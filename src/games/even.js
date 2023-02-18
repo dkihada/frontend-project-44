17a03b33d474c45dfca1100fce7d74c9abb1bdc5
Denis Agureev <dkihada@gmail.com>
2023-02-18 20:23:49 +0500

    Игра: "Калькулятор"

--- src/games/even.js
+++ src/games/even.js
@@ -1,42 +1,18 @@
-import readlineSync from 'readline-sync';
+import runGame from '../index.js';
+import {
+  getRandomNumber,
+  isEvenNumber,
+} from '../utils.js';
 
-const incorrectAnswer = (name) => `'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${name}!`;
+const description = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const isEven = (number) => (number % 2 === 0);
+const getQuestionAndAnswer = () => {
+  const question = getRandomNumber(0, 100);
+  const correctAnswer = isEvenNumber(question) ? 'yes' : 'no';
+
+  return [question, correctAnswer];
+};
 
 export default () => {
-  console.log('Welcome to the Brain Games!');
-
-  const playerName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${playerName}!`);
-
-  const count = 3;
-  console.log('Answer "yes" if the randomNumber is even, otherwise answer "no".');
-  let index = 0;
-
-  while (index < count) {
-    const randomNumber = Math.floor(Math.random(10) * 10);
-    console.log(`Question: ${randomNumber}`);
-    const answer = readlineSync.question('Your answer: ');
-
-    if (answer.toLowerCase() === 'yes') {
-      if (isEven(randomNumber)) {
-        console.log('Correct!');
-        index += 1;
-      } else {
-        console.log(incorrectAnswer(playerName));
-        index = 0;
-      }
-    } else if (answer.toLowerCase() === 'no') {
-      if (!isEven(randomNumber)) {
-        console.log('Correct!');
-        index += 1;
-      } else {
-        console.log(incorrectAnswer(playerName));
-        index = 0;
-      }
-    }
-  }
-
-  console.log(`Congratulations, ${playerName}!`);
+  runGame(description, getQuestionAndAnswer);
 };
---
+++ src/utils.js
@@ -0,0 +1,3 @@
+export const getRandomNumber = (minRange, maxRange) => Math.floor(Math.random() * (maxRange - minRange) + minRange);
+
+export const isEvenNumber = (number) => number % 2 === 0;
